b2c8c62617e8afe71c51a8278fdf6f6bb8828a6f
wajahatalli <wajahat.jutt09@gmail.com>
2022-11-15 17:25:32 +0500

    Add functions in js file

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const close_btn = document.querySelector('.close-btn');
 let openMenu = document.querySelector( '.menu-bar');
 const menu_items2 = document.querySelectorAll('nav .menu-items li a');
 
+openMenu.addEventListener('click',show);
+close_btn.addEventListener('click',close);
+
+// close menu when you click on a menu item
+menu_items2.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    menu_items.style.display = 'block';
+    menu_items.style.top = '0';
+}
+
+function close(){
+    menu_items.style.top = '-100%';
+}
