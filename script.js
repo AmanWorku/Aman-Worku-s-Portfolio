c07a7d3dc4173ec6c3317ae68f439b6cd7d91ab7
AmanWorku <amexworku2014@gmail.com>
2022-11-15 15:22:24 +0300

    edited some part of javascript

--- script.js
+++ script.js
@@ -3,23 +3,3 @@ const close_btn = document.querySelector('.close-btn');
 let openMenu = document.querySelector( '.menu-bar');
 const menu_items2 = document.querySelectorAll('nav .menu-items li a');
 
-function show(){
-    console.log('clicked');
-    menu_items.style.display = 'block';
-    menu_items.style.top = '0';
-}
-
-openMenu.addEventListener('click',show);
-close_btn.addEventListener('click',close);
-
-// close menu when you click on a menu item
-menu_items2.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
-})
-
-
-function close(){
-    menu_items.style.top = '-100%';
-}
